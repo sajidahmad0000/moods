ea4b07173a262f255126d6daea75a6f39f124bb9
Sajid Ahmad <sajid01ahmad@gmail.com>
2024-08-16 11:44:49 +0530

    login component sorted

--- components/Login.js
+++ components/Login.js
@@ -10,15 +10,19 @@ export default function Login() {
       <h3 className={" text-4xl " + fugaz.className}>Log In / Register</h3>;
       <p>You&#39;re one step away</p>
       <input
-        className="w-full max-w-[400px] mx-auto px-4 py-2 sm:py-3 border border-solid border-indigo-400 "
+        className=" w-full max-w-[400px] mx-auto px-4 py-2 sm:py-3 border border-solid border-indigo-400 rounded-full outline-none duration-200 hover:border-indigo-600 focus:border-indigo-600 "
         placeholder="Email"
       />
       <input
-        className="w-full max-w-[400px] mx-auto px-4 py-2 sm:py-3 border border-solid border-indigo-400 "
+        className="w-full max-w-[400px] mx-auto px-4 py-2 sm:py-3 border border-solid border-indigo-400 rounded-full outline-none  duration-200 hover:border-indigo-600 focus:border-indigo-600"
         placeholder="Password"
         type="password"
       />
       <Button text="Submit" full />
+      <p className="text-center">
+        Don&#39;t have an account ?{" "}
+        <span className="text-indigo-600 font-semibold">Sign Up</span>
+      </p>
     </div>
   );
 }
